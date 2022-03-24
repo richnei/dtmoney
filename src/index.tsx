2cde4565a36be93f81035ec00139a3265081d708
Richnei <rich.nei@hotmail.com>
2022-03-24 13:35:18 -0300

    form

--- src/index.tsx
+++ src/index.tsx
@@ -7,6 +7,30 @@ createServer({
   models: {
     transaction: Model,
   },
+
+  seeds(server) {
+    server.db.loadData({
+      transactions: [
+        {
+          id: 1,
+          title: "Freelance de website",
+          type: "deposit",
+          category: "Dev",
+          amount: 6080,
+          createAt: new Date('2022-02-12 09:00:00')
+        },
+        {
+          id: 2,
+          title: "Aluguel",
+          type: "withdraw",
+          category: "Casa",
+          amount: 6080,
+          createAt: new Date('2022-02-14 11:00:00')
+        }
+      ]
+    })
+  },
+
   routes() {
     this.namespace = 'api';
 
