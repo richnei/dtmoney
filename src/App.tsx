aacb72ee1bd62607ff59c77c15d440e24c6c4f38
Richnei <rich.nei@hotmail.com>
2022-03-24 16:51:50 -0300

    contx

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,9 @@ import Modal from "react-modal";
 import { Dashboard } from "./components/Dashboard";
 import { Header } from "./components/Header";
 import { NewTransactionModal } from "./components/NewTransactionModal";
+import { TransactionsContext } from "./TransactionsContext";
 import { GlobalStyle } from "./styles/global";
+
 
 Modal.setAppElement("#root");
 
@@ -20,7 +22,7 @@ export function App() {
   }
 
   return (
-    <>
+    <TransactionsContext.Provider value={[]}>
       <Header onOpenNewTransactionModal={handleOpenNewTransactionModal} />
       <Dashboard />
       <NewTransactionModal
@@ -28,7 +30,7 @@ export function App() {
         onRequestClose={handleCloseNewTransactionModal}
       />
       <GlobalStyle />
-    </>
+    </TransactionsContext.Provider>
   );
 }
 
