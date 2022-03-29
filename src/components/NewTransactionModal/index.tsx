03c817016bd693cf427c05be9f0307b3dc9c2759
Richnei <rich.nei@hotmail.com>
2022-03-29 15:21:13 -0300

    calc

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -23,15 +23,21 @@ export function NewTransactionModal({ isOpen, onRequestClose }: NewTransactionMo
     const [category, setCategory] = useState('');
     const [type, setType] = useState('deposit');
 
-    function handleCreateNewTransaction(event: FormEvent) {
+    async function handleCreateNewTransaction(event: FormEvent) {
         event.preventDefault();
 
-        createTransaction({
+        await createTransaction({
             title,
             amount,
             category,
             type,
         })
+
+        setTitle('');
+        setAmount(0);
+        setCategory('');
+        setType('deposit');
+        onRequestClose();
     }
     return (
         <Modal
@@ -98,7 +104,7 @@ export function NewTransactionModal({ isOpen, onRequestClose }: NewTransactionMo
 
 
 
-            </Container> 
+            </Container>
         </Modal >
     );
 }
